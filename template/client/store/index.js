eb0474ca315d419ae6d9c1183bc78b68935caca2
hpgood <35557088@qq.com>
2018-10-09 18:45:32 +0800

    Update index.js

--- template/client/store/index.js
+++ template/client/store/index.js
@@ -4,24 +4,53 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  count: 0
+  token: '',
+  base: 'https://login.k12china.com',
+  url: 'https://www.k12china.com',
+  username: '',
+  sysusername: '',
+  userid: -1
 }
 
 const mutations = {
-  INCREMENT (state) {
-    state.count++
+  saveToken (state, code) {
+    state.token = code
   },
-  DECREMENT (state) {
-    state.count--
+  saveUserName (state, username) {
+    state.username = username
+  },
+  saveUser (state, obj) {
+    state.realname = obj.real_name
+    state.sysusername = obj.user_name
+    state.userid = obj.user_id
   }
 }
 
 const actions = {
   incrementAsync ({ commit }) {
     setTimeout(() => {
-      commit('INCREMENT')
+      //commit('INCREMENT')
     }, 200)
   }
+}
+
+const href = window.location.href
+// debug
+if (href.indexOf('http://127.0.0.1') === 0 || href.indexOf('http://localhost') === 0) {
+  console.log('run in 本机调试')
+  state.base = 'http://192.168.6.30:32390'
+  // state.base = 'http://127.0.0.1:8080'
+  state.url = 'http://127.0.0.1:8080'
+} else if (href.indexOf('http://192.168.6.30') === 0 || href.indexOf('http://192.168.6.31') === 0) {
+  console.log('run in 开发环境')
+  state.base = 'http://192.168.6.30:32390'
+  state.url = ''
+} else if (href.indexOf('http://192.168.6.35') === 0 || href.indexOf('http://192.168.6.36') === 0) {
+  console.log('run in 测试环境')
+  state.base = 'http://login.k12china-test.com'
+  state.url = ''
+} else {
+  console.log('run in 生产环境')
 }
 
 const store = new Vuex.Store({
