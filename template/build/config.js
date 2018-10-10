1e29697859e55e6033b5039fbb35acd0dfcd5d81
hpgood <35557088@qq.com>
2018-10-10 09:45:17 +0800

    发布输出的文件夹

--- template/build/config.js
+++ template/build/config.js
@@ -9,6 +9,8 @@ module.exports = {
     host: 'localhost',
     port: 4000
   },
+  //发布输出的文件夹
+  dist: '../src/dist',
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
   publicPath: '{{#if electron}}.{{/if}}/web/',{{#if electron}}
