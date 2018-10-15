2e961cd8412d5908b04e8a0b513bffafcdbee809
hpgood <35557088@qq.com>
2018-10-15 19:20:54 +0800

    Update config.js

--- template/build/config.js
+++ template/build/config.js
@@ -11,10 +11,11 @@ module.exports = {
   },
   //发布输出的文件夹
   dist: '../src/dist',
+  //postcss模块化
+  cssModules: true,
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
   publicPath: '{{#if electron}}.{{/if}}/web/',{{#if electron}}
   electron: true,{{/if}}{{#if jsx}}
-  cssModules: true,
   jsx: true{{/if}}
 }
